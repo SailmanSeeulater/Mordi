cd97722bc488d7bd85bd1aeb4f49eeaa6be7251d
SailmanSeeulater <latersailor@gmail.com>
2026-09-08 00:10:25 -0700

    Allowing Swagger access

--- backend/src/main/java/com/mordi/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/mordi/backend/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth-> auth
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/actuator/health/**", "/actuator/info").permitAll()
+                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated())
             .addFilterBefore(rateLimitFilter, UsernamePasswordAuthenticationFilter.class)
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
